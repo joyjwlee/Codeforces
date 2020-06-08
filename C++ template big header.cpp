41cff0c2e38d5f53f75427cf926e6d1e54be626b
Jaewon Lee <joyjwlee@gmail.com>
2020-06-08 10:51:18 -0700

    template

--- C++ template big header.cpp
+++ C++ template big header.cpp
@@ -3,12 +3,15 @@
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
 #define MOD 1000000007
-#define test int t; cin>>t; while(t--)
-#define init(arr,val) memset(arr,val,sizeof(arr))
-#define loop(i,a,b) for(int i=a;i<b;i++)
-#define loopr(i,a,b) for(int i=a;i>=b;i--)
-#define loops(i,a,b,step) for(int i=a;i<b;i+=step)
-#define looprs(i,a,b,step) for(int i=a;i>=b;i-=step)
+#define test  \
+    int t;    \
+    cin >> t; \
+    while (t--)
+#define init(arr, val) memset(arr, val, sizeof(arr))
+#define loop(i, a, b) for (int i = a; i < b; i++)
+#define loopr(i, a, b) for (int i = a; i >= b; i--)
+#define loops(i, a, b, step) for (int i = a; i < b; i += step)
+#define looprs(i, a, b, step) for (int i = a; i >= b; i -= step)
 #define ull unsigned long long int
 #define ll long long int
 #define P pair
@@ -30,8 +33,11 @@
 #define S second
 #define IT iterator
 #define RIT reverse_iterator
-#define FAST ios_base::sync_with_stdio(false);cin.tie();cout.tie();
-#define FILE_READ_IN freopen("input.txt","r",stdin);
-#define FILE_READ_OUT freopen("output.txt","w",stdout);
-#define all(a) a.begin(),a.end()
+#define FAST                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie();                        \
+    cout.tie();
+#define FILE_READ_IN freopen("input.txt", "r", stdin);
+#define FILE_READ_OUT freopen("output.txt", "w", stdout);
+#define all(a) a.begin(), a.end()
 #define ld long double
